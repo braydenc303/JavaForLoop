6bf77cd774f3f86b443614678b029041785e6de4
Brayden Copley <braydenc303@gmail.com>
2019-10-12 07:00:16 -0600

    first loop

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,6 +2,13 @@ package academy.learnprogramming;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        for(int i = 2; i < 5; i++) {
+            System.out.println("10000 at " + i +"% interest = " + calculateInterest(10000.0, i));
+        }
+ 
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return amount * (interestRate/100);
     }
 }
