7e74ef8710eda2c2cce9442f3211bdceeb7d5d8c
Brayden Copley <braydenc303@gmail.com>
2019-10-12 07:26:09 -0600

    challenges complete

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,10 +2,39 @@ package academy.learnprogramming;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        for(int i = 2; i < 5; i++) {
-            System.out.println("10000 at " + i +"% interest = " + calculateInterest(10000.0, i));
+        for(int i = 8; i > 1; i--) {
+            System.out.println("10000 at " + i +"% interest = " + String.format("%.2f", calculateInterest(10000.0, (double) i)));
+        }        
+        System.out.println("------------------------");
+        for(int i = 2; i < 9; i++) {
+            System.out.println("10000 at " + i +"% interest = " + String.format("%.2f", calculateInterest(10000.0, (double) i)));
+        }
+
+        int count = 0;
+        
+        for(int i = 2; i < 15; i++) {
+            if(isPrime(i)) {
+                System.out.println(i);
+                count++;
+                if(count ==3) {
+                    break;
+                }
+            }
         }
  
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i=2; i<=n/2; i++) {
+            if(n%i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
